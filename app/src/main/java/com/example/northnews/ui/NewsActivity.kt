3a9bc9f8f5faf2b73f15b437ceefcf5ab2a2b850
Nelson <nelson.goncalves@safetonet.com>
2022-10-31 15:57:47 +0000

    Created navGraph Created animations for navGraph Setup NavController

--- app/src/main/java/com/example/northnews/ui/NewsActivity.kt
+++ app/src/main/java/com/example/northnews/ui/NewsActivity.kt
@@ -2,15 +2,30 @@ package com.example.northnews.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.northnews.R
 import com.example.northnews.databinding.ActivityNewsBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNewsBinding
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.activityNewsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavView = findViewById(R.id.activityNewsBottomNavigation)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
     }
 }
--- app/src/main/java/com/example/northnews/ui/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/northnews/ui/fragments/NewsFragment.kt
@@ -3,5 +3,5 @@ package com.example.northnews.ui.fragments
 import androidx.fragment.app.Fragment
 import com.example.northnews.R
 
-class BreakingNewsFragment: Fragment(R.layout.fragment_news_list) {
+class NewsFragment: Fragment(R.layout.fragment_news_list) {
 }
